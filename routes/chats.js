442827447dbfa50cd97109d79681670c6c1d0872
Raul Rohjans <rauljann@hotmail.com>
2022-04-25 19:03:12 +0100

    Added the main chat interface as well as some minor features to it

--- routes/chats.js
+++ routes/chats.js
@@ -33,27 +33,58 @@ const upload = multer({
     },
 })
 
-router.get('/', (req, res) => {
+router.get('/', authenticateToken, async (req, res) => {
+    //Get chats from DB
+    let queryResult = await new Promise(async (resolve, reject) => {
+        connection.query("SELECT * FROM chat inner join chat_user on chat.idChat=chat_user.idChat WHERE chat_user.idUser" +
+            " = ? and chat.deleted = 0",
+            [
+                req.user.idUser
+            ],
+            (err, result, fields) => {
+                resolve({err: err, result: result})
+            })
+    })
+
+    if (queryResult.err) {
+        res.status(500).send("An error has occurred while loading the page.\n" + queryResult.err.code + ":\n"
+        + queryResult.err.sqlMessage)
+        return
+    }
+
+
+    res.render("chats/chats", {chats: queryResult.result})
+});
+
+router.get('/new', authenticateToken, async (req, res) => {
     //Redirect to login page if not logged in
-    if(!isLoggedIn(req)){
+    if (!isLoggedIn(req)) {
         res.redirect('/login')
         return
     }
 
-    res.render("chats/chats")
-});
+    //Get users from DB
+    let queryResult = await new Promise(async (resolve, reject) => {
+        connection.query("SELECT idUser, username FROM user WHERE idUser != ? and deleted = 0",
+            [
+                req.user.idUser
+            ],
+            (err, result, fields) => {
+                resolve({err: err, result: result})
+            })
+    })
 
-router.get('/new', (req, res) => {
-    //Redirect to login page if not logged in
-    if(!isLoggedIn(req)){
-        res.redirect('/login')
+    if (queryResult.err) {
+        res.status(500).send("An error has occurred while loading the page.\n" + queryResult.err.code + ":\n"
+            + queryResult.err.sqlMessage)
         return
     }
 
-    res.render("chats/new-chat")
+
+    res.render("chats/new-chat", {users: queryResult.result})
 });
 
-router.post('/new', upload.single('file_logo'), async (req, res) => {
+router.post('/new', authenticateToken, upload.single('file_logo'), async (req, res) => {
     let queryResult;
 
     //Redirect to login page if not logged in
@@ -117,12 +148,14 @@ router.post('/new', upload.single('file_logo'), async (req, res) => {
     }
 
     //Add users to chat
+    let userList = req.body.usrSelect;
+    userList.push(req.user.idUser);
     for(let i = 0; i < req.body.usrSelect.length; i++){
 
         queryResult = await new Promise(async (resolve, reject) => {
             connection.query("INSERT INTO chat_user(idUser, idChat, useradded_date) VALUES(?, ?, ?)",
                 [
-                    req.body.usrSelect[0],
+                    userList[i],
                     newID,
                     new Date()
                 ],
@@ -140,6 +173,73 @@ router.post('/new', upload.single('file_logo'), async (req, res) => {
     res.render("chats/new-chat", {successMessage: "Chat created successfully!"})
 });
 
+router.get('/:chatId', authenticateToken, async (req, res) => {
+
+    //Check if chat exists
+    let queryResult = await new Promise(async (resolve, reject) => {
+        connection.query("SELECT * FROM chat where idChat = ? and deleted = 0",
+            [
+                req.params.chatId
+            ],
+            (err, result, fields) => {
+                resolve({err: err, result: result})
+            })
+    })
+
+    if (queryResult.err || queryResult.result.length < 1) {
+        res.redirect('/chats')
+        return
+    }
+    let chatObj = queryResult.result[0]
+
+    //Check if user is in chat
+    queryResult = await new Promise(async (resolve, reject) => {
+        connection.query("SELECT COUNT(*) as chatUserCount FROM chat_user where idChat = ? and idUser = ?",
+            [
+                req.params.chatId,
+                req.user.idUser
+            ],
+            (err, result, fields) => {
+                resolve({err: err, result: result})
+            })
+    })
+
+    if (queryResult.err || queryResult.result[0].chatUserCount < 1) {
+        res.redirect('/chats')
+        return
+    }
+
+    //Get users in chat
+    queryResult = await new Promise(async (resolve, reject) => {
+        connection.query("SELECT * FROM user INNER JOIN chat_user ON user.idUser = chat_user.idUser" +
+            " INNER JOIN chat ON chat_user.idChat = chat.idChat" +
+            " WHERE chat_user.idChat = ? and chat.deleted = 0 and user.deleted = 0",
+            [
+                req.params.chatId
+            ],
+            (err, result, fields) => {
+                resolve({err: err, result: result})
+            })
+    })
+
+    if (queryResult.err || queryResult.result.length < 1) {
+        res.redirect('/chats')
+        return
+    }
+    let userObjs = queryResult.result
+
+    res.render("chats/chat", {chat: chatObj, users: userObjs})
+})
+
+router.get('/:chatId/leave', authenticateToken, (req, res) => {
+    //Check if chat exists
+
+
+    //Display error when user is not in chat
+
+    res.send('Leave chat ' + req.params.chatId);
+})
+
 //Functions
 function isLoggedIn(req) {
     if (!req.cookies)
@@ -152,6 +252,23 @@ function isLoggedIn(req) {
     //Check if cookie is valid
     return jwt.verify(req.cookies['AuthToken'], process.env.ACCESS_TOKEN_SECRET, (err, user) => {
         return !err;
+    }, null)
+}
+
+function authenticateToken(req, res, next) {
+    if(!req.cookies)
+        return res.redirect('/login')
+
+    if(!req.cookies['AuthToken'])
+        return res.redirect('/login')
+
+
+    jwt.verify(req.cookies['AuthToken'], process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+        if(err)
+            return res.redirect('/login?code=1011')
+
+        req.user = user;
+        next()
     }, null)
 }
 
